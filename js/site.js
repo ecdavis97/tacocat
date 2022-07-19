0396a519486dd7fb8196f7903610b5bd253815ef
ecdavis97 <ethancdavis97@gmail.com>
2022-07-19 16:25:11 -0400

    init commit

--- js/site.js
+++ js/site.js
@@ -1,17 +1,41 @@
+//controller function
 function getValue(){
-    alert("Hello World!")
+    message = document.getElementById("message").value;
+    let revMessage = reverse(message);
+    displayReversed(revMessage,message);
 }
 
-function getValue2(){
-    let msg='';
 
-    msg=document.getElementById("message").value;
+//business logic
+function reverse(message){
+    //string? an array of character
+    let reversedMessage = "";
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    //decrementing for loop
+    for (let index = message.length-1; index >=0; index--) {
+        
+        reversedMessage += message[index];
+        
+    }
+
+    return reversedMessage;
+
+}
+
+
+//display the reversed string 
+function displayReversed(revMessage,message){
+    let results = document.getElementById("results");
+    results.classList.add("revMessage"); //this adds the ability to change the style through CSS
+    
+    let displayMessage = "";
+
+    if (revMessage.toLowerCase() == message.toLowerCase()) {
+        //display "YEAH cool palindrome!"
+        displayMessage = "YEAH! " + message + " is a cool palindrome!";
+    } else {
+        //display "That's not a cool palindrome"
+        displayMessage = message + " is not a cool palindrome.";
+    }
+    results.innerHTML = displayMessage;
 }
